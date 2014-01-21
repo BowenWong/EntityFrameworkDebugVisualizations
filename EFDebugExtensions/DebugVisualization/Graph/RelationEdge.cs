155aafc5ef385d4d5b6abec10f21e25849a2242d
andyp <andy.pelzer@gmx.de>
2014-01-21 20:42:27 +0100

    More informative text for relation multiplicity

--- EFDebugExtensions/DebugVisualization/Graph/RelationEdge.cs
+++ EFDebugExtensions/DebugVisualization/Graph/RelationEdge.cs
@@ -31,11 +31,37 @@ namespace EntityFramework.Debug.DebugVisualization.Graph
 
             State = EntityState.Unchanged;
             DeleteBehavior = navigationProperty.ToEndMember.DeleteBehavior;
-
-#warning display multiplicity a bit more informative: e.g. ZeroOrOne 'EntityName' to Many 'EntitySetName'
-
-            Multiplicity = String.Format("{0}-to-{1}", navigationProperty.FromEndMember.RelationshipMultiplicity, navigationProperty.ToEndMember.RelationshipMultiplicity);
+            Multiplicity = GetHumanReadableMultiplicity(navigationProperty);
             Name = navigationProperty.Name;
+        }
+
+        private string GetHumanReadableMultiplicity(NavigationProperty navigationProperty)
+        {
+            return string.Format("{0} {1} to {2} {3}",
+                                 HumanReadableMultiplicity(navigationProperty.FromEndMember.RelationshipMultiplicity),
+                                 GetEntityName(Source, navigationProperty.FromEndMember.RelationshipMultiplicity),
+                                 HumanReadableMultiplicity(navigationProperty.ToEndMember.RelationshipMultiplicity),
+                                 GetEntityName(Target, navigationProperty.ToEndMember.RelationshipMultiplicity));
+        }
+
+        private static string GetEntityName(EntityVertex entityVertex, RelationshipMultiplicity multiplicity)
+        {
+            return multiplicity == RelationshipMultiplicity.One ? entityVertex.TypeName : entityVertex.EntitySetName;
+        }
+
+        private static string HumanReadableMultiplicity(RelationshipMultiplicity multiplicity)
+        {
+            switch (multiplicity)
+            {
+                case RelationshipMultiplicity.ZeroOrOne:
+                    return "zero or one";
+                case RelationshipMultiplicity.One:
+                    return "one";
+                case RelationshipMultiplicity.Many:
+                    return "many";
+                default:
+                    throw new ArgumentOutOfRangeException("multiplicity");
+            }
         }
     }
 }
