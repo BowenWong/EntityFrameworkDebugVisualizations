008171584d116ef58ec9b5925da4f5548b293f32
andyp <andy.pelzer@gmx.de>
2014-01-22 21:06:08 +0100

    Added tests for parallel relations

---
+++ EFDebugExtensions.UnitTests/Tests/ParallelRelationBehaviors.cs
@@ -0,0 +1,190 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using EntityFramework.Debug.UnitTests.Infrastructure;
+using EntityFramework.Debug.UnitTests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EntityFramework.Debug.UnitTests.Tests
+{
+    [TestClass]
+    public class ParallelRelationBehaviors : Testbase
+    {
+        [TestMethod]
+        public void ShouldSeeAddedParallelRelations()
+        {
+            using (var context = new TestDbContext())
+            {
+                var child = new EntityWithChild();
+                context.EntitiesWithChild.Add(child);
+                var parent = new EntityWithChild();
+                context.EntitiesWithChild.Add(parent);
+                context.SaveChanges();
+
+                parent.FavoriteChild = child;
+                parent.Children.Add(child);
+
+                var vertices = context.GetEntityVertices();
+
+                Assert.AreEqual(2, vertices.Count);
+                var parentVertex = GetVertexByIdProperty(vertices, parent.Id);
+                var childVertex = GetVertexByIdProperty(vertices, child.Id);
+
+                Assert.AreEqual(2, parentVertex.Relations.Count(r => r.State == EntityState.Added));
+                Assert.IsTrue(parentVertex.Relations.All(r => r.Target == childVertex));
+
+                Assert.AreEqual(0, childVertex.Relations.Count());
+            }
+        }
+
+        [TestMethod]
+        public void ShouldSeeUnchangedParallelRelations()
+        {
+            using (var context = new TestDbContext())
+            {
+                var child = new EntityWithChild();
+                context.EntitiesWithChild.Add(child);
+                var parent = new EntityWithChild();
+                context.EntitiesWithChild.Add(parent);
+                context.SaveChanges();
+
+                parent.FavoriteChild = child;
+                parent.Children.Add(child);
+                context.SaveChanges();
+
+                var vertices = context.GetEntityVertices();
+
+                Assert.AreEqual(2, vertices.Count);
+                var parentVertex = GetVertexByIdProperty(vertices, parent.Id);
+                var childVertex = GetVertexByIdProperty(vertices, child.Id);
+
+                Assert.AreEqual(2, parentVertex.Relations.Count(r => r.State == EntityState.Unchanged));
+                Assert.IsTrue(parentVertex.Relations.All(r => r.Target == childVertex));
+
+                Assert.AreEqual(0, childVertex.Relations.Count());
+            }
+        }
+
+        [TestMethod]
+        public void ShouldSeeRemovedParallelRelations()
+        {
+            using (var context = new TestDbContext())
+            {
+                var child = new EntityWithChild();
+                context.EntitiesWithChild.Add(child);
+                var parent = new EntityWithChild();
+                context.EntitiesWithChild.Add(parent);
+                context.SaveChanges();
+
+                parent.FavoriteChild = child;
+                parent.Children.Add(child);
+                context.SaveChanges();
+
+                parent.FavoriteChild = null;
+                parent.Children.Remove(child);
+
+                var vertices = context.GetEntityVertices();
+
+                Assert.AreEqual(2, vertices.Count);
+                var parentVertex = GetVertexByIdProperty(vertices, parent.Id);
+                var childVertex = GetVertexByIdProperty(vertices, child.Id);
+
+                Assert.AreEqual(2, parentVertex.Relations.Count(r => r.State == EntityState.Deleted));
+                Assert.IsTrue(parentVertex.Relations.All(r => r.Target == childVertex));
+
+                Assert.AreEqual(0, childVertex.Relations.Count());
+            }
+        }
+
+        [TestMethod]
+        public void ShouldSeeAddedAndUnchangedParallelRelations()
+        {
+            using (var context = new TestDbContext())
+            {
+                var child = new EntityWithChild();
+                context.EntitiesWithChild.Add(child);
+                var parent = new EntityWithChild();
+                context.EntitiesWithChild.Add(parent);
+                context.SaveChanges();
+
+                parent.FavoriteChild = child;
+                context.SaveChanges();
+
+                parent.Children.Add(child);
+
+                var vertices = context.GetEntityVertices();
+
+                Assert.AreEqual(2, vertices.Count);
+                var parentVertex = GetVertexByIdProperty(vertices, parent.Id);
+                var childVertex = GetVertexByIdProperty(vertices, child.Id);
+
+                Assert.AreEqual(1, parentVertex.Relations.Count(r => r.State == EntityState.Unchanged));
+                Assert.AreEqual(1, parentVertex.Relations.Count(r => r.State == EntityState.Added));
+                Assert.IsTrue(parentVertex.Relations.All(r => r.Target == childVertex));
+
+                Assert.AreEqual(0, childVertex.Relations.Count());
+            }
+        }
+
+        [TestMethod]
+        public void ShouldSeeAddedAndRemovedParallelRelations()
+        {
+            using (var context = new TestDbContext())
+            {
+                var child = new EntityWithChild();
+                context.EntitiesWithChild.Add(child);
+                var parent = new EntityWithChild();
+                context.EntitiesWithChild.Add(parent);
+                context.SaveChanges();
+
+                parent.FavoriteChild = child;
+                context.SaveChanges();
+
+                parent.Children.Add(child);
+                parent.FavoriteChild = null;
+
+                var vertices = context.GetEntityVertices();
+
+                Assert.AreEqual(2, vertices.Count);
+                var parentVertex = GetVertexByIdProperty(vertices, parent.Id);
+                var childVertex = GetVertexByIdProperty(vertices, child.Id);
+
+                Assert.AreEqual(1, parentVertex.Relations.Count(r => r.State == EntityState.Deleted));
+                Assert.AreEqual(1, parentVertex.Relations.Count(r => r.State == EntityState.Added));
+                Assert.IsTrue(parentVertex.Relations.All(r => r.Target == childVertex));
+
+                Assert.AreEqual(0, childVertex.Relations.Count());
+            }
+        }
+
+        [TestMethod]
+        public void ShouldSeeUnchangedAndRemovedParallelRelations()
+        {
+            using (var context = new TestDbContext())
+            {
+                var child = new EntityWithChild();
+                context.EntitiesWithChild.Add(child);
+                var parent = new EntityWithChild();
+                context.EntitiesWithChild.Add(parent);
+                context.SaveChanges();
+
+                parent.FavoriteChild = child;
+                parent.Children.Add(child);
+                context.SaveChanges();
+
+                parent.Children.Remove(child);
+
+                var vertices = context.GetEntityVertices();
+
+                Assert.AreEqual(2, vertices.Count);
+                var parentVertex = GetVertexByIdProperty(vertices, parent.Id);
+                var childVertex = GetVertexByIdProperty(vertices, child.Id);
+
+                Assert.AreEqual(1, parentVertex.Relations.Count(r => r.State == EntityState.Unchanged));
+                Assert.AreEqual(1, parentVertex.Relations.Count(r => r.State == EntityState.Deleted));
+                Assert.IsTrue(parentVertex.Relations.All(r => r.Target == childVertex));
+
+                Assert.AreEqual(0, childVertex.Relations.Count());
+            }
+        }
+    }
+}
