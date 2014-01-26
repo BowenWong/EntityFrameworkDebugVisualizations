83ea4a5d8ef7ba94756ccd78aa7c2630d96456c6
andypelzer <andy.pelzer@gmx.de>
2014-01-26 12:40:10 +0100

    GraphX is generally working (a lot of stuff still isn't)

--- EntityFrameworkDebugVisualizations/DebugVisualization/Views/GraphArea.xaml.cs
+++ EntityFrameworkDebugVisualizations/DebugVisualization/Views/GraphArea.xaml.cs
@@ -32,18 +32,34 @@ namespace EntityFramework.Debug.DebugVisualization.Views
             InitializeComponent();
 
             var logicCore = new Logic();
-            logicCore.DefaultLayoutAlgorithm = LayoutAlgorithmTypeEnum.ISOM;
+            //This property sets layout algorithm that will be used to calculate vertices positions
+            //Different algorithms uses different values and some of them uses edge Weight property.
+            logicCore.DefaultLayoutAlgorithm = LayoutAlgorithmTypeEnum.KK;
+            //Now we can set optional parameters using AlgorithmFactory
             logicCore.DefaultLayoutAlgorithmParams =
-                               logicCore.AlgorithmFactory.CreateLayoutParameters(LayoutAlgorithmTypeEnum.ISOM);
-            //((ISOMLayoutParameters)logicCore.DefaultLayoutAlgorithmParams).MaxIterations = 100;
+                               logicCore.AlgorithmFactory.CreateLayoutParameters(LayoutAlgorithmTypeEnum.KK);
+            //Unfortunately to change algo parameters you need to specify params type which is different for every algorithm.
+            ((KKLayoutParameters)logicCore.DefaultLayoutAlgorithmParams).MaxIterations = 100;
+
+            //This property sets vertex overlap removal algorithm.
+            //Such algorithms help to arrange vertices in the layout so no one overlaps each other.
             logicCore.DefaultOverlapRemovalAlgorithm = OverlapRemovalAlgorithmTypeEnum.FSA;
+            //Setup optional params
             logicCore.DefaultOverlapRemovalAlgorithmParams =
                               logicCore.AlgorithmFactory.CreateOverlapRemovalParameters(OverlapRemovalAlgorithmTypeEnum.FSA);
             ((OverlapRemovalParameters)logicCore.DefaultOverlapRemovalAlgorithmParams).HorizontalGap = 50;
             ((OverlapRemovalParameters)logicCore.DefaultOverlapRemovalAlgorithmParams).VerticalGap = 50;
+
+            //This property sets edge routing algorithm that is used to build route paths according to algorithm logic.
+            //For ex., SimpleER algorithm will try to set edge paths around vertices so no edge will intersect any vertex.
             logicCore.DefaultEdgeRoutingAlgorithm = EdgeRoutingAlgorithmTypeEnum.SimpleER;
+
+            //This property sets async algorithms computation so methods like: Area.RelayoutGraph() and Area.GenerateGraph()
+            //will run async with the UI thread. Completion of the specified methods can be catched by corresponding events:
+            //Area.RelayoutFinished and Area.GenerateGraphFinished.
             logicCore.AsyncAlgorithmCompute = false;
 
+            //Finally assign logic core to GraphArea object
             Area.LogicCore = logicCore;
         }
     }
