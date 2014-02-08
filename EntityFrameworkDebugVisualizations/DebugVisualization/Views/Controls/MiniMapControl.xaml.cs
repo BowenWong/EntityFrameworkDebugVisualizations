248d85ddb76e8ba26d2a476a196a0be7eec80f08
andypelzer <andy.pelzer@gmx.de>
2014-02-08 14:38:00 +0100

    The size of the mini-map now depends on the size of the content (scale is fix)

--- EntityFrameworkDebugVisualizations/DebugVisualization/Views/Controls/MiniMapControl.xaml.cs
+++ EntityFrameworkDebugVisualizations/DebugVisualization/Views/Controls/MiniMapControl.xaml.cs
@@ -10,12 +10,46 @@ namespace EntityFramework.Debug.DebugVisualization.Views.Controls
         #region ContentVisual
 
         public static readonly DependencyProperty ContentVisualProperty = DependencyProperty.Register(
-                "ContentVisual", typeof(FrameworkElement), typeof(MiniMapControl), new PropertyMetadata(default(FrameworkElement)));
+                "ContentVisual", typeof(FrameworkElement), typeof(MiniMapControl), new PropertyMetadata(default(FrameworkElement), OnContentVisualChanged));
 
         public FrameworkElement ContentVisual
         {
             get { return (FrameworkElement)GetValue(ContentVisualProperty); }
             set { SetValue(ContentVisualProperty, value); }
+        }
+
+        private static void OnContentVisualChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var miniMapControl = (MiniMapControl)d;
+
+            DependencyPropertyDescriptor prop = DependencyPropertyDescriptor.FromProperty(ActualWidthProperty, typeof(FrameworkElement));
+            if (e.OldValue != null)
+                prop.RemoveValueChanged(e.OldValue, miniMapControl.UpdateVisibleAreaIndicator);
+            prop.AddValueChanged(miniMapControl.ContentVisual, miniMapControl.UpdateVisibleAreaIndicator);
+
+            prop = DependencyPropertyDescriptor.FromProperty(ActualHeightProperty, typeof(FrameworkElement));
+            if (e.OldValue != null)
+                prop.RemoveValueChanged(e.OldValue, miniMapControl.UpdateVisibleAreaIndicator);
+            prop.AddValueChanged(miniMapControl.ContentVisual, miniMapControl.UpdateVisibleAreaIndicator);
+        }
+
+        #endregion
+
+        #region MiniMapScale
+
+        public static readonly DependencyProperty MiniMapScaleProperty = DependencyProperty.Register(
+                "MiniMapScale", typeof(double), typeof(MiniMapControl), new PropertyMetadata(default(double), OnMiniMapScaleChanged));
+
+        public double MiniMapScale
+        {
+            get { return (double)GetValue(MiniMapScaleProperty); }
+            set { SetValue(MiniMapScaleProperty, value); }
+        }
+
+        private static void OnMiniMapScaleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var miniMapControl = (MiniMapControl)d;
+            miniMapControl.UpdateVisibleAreaIndicator(miniMapControl, EventArgs.Empty);
         }
 
         #endregion
@@ -31,16 +65,9 @@ namespace EntityFramework.Debug.DebugVisualization.Views.Controls
             set { SetValue(ZoomControlProperty, value); }
         }
 
-        #endregion
-
-        public MiniMapControl()
-        {
-            InitializeComponent();
-        }
-
         private static void OnZoomControlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var miniMapControl = (MiniMapControl) d;
+            var miniMapControl = (MiniMapControl)d;
 
             DependencyPropertyDescriptor prop = DependencyPropertyDescriptor.FromProperty(ZoomControl.TranslateXProperty, typeof(ZoomControl));
             if (e.OldValue != null)
@@ -56,15 +83,40 @@ namespace EntityFramework.Debug.DebugVisualization.Views.Controls
             if (e.OldValue != null)
                 prop.RemoveValueChanged(e.OldValue, miniMapControl.UpdateVisibleAreaIndicator);
             prop.AddValueChanged(miniMapControl.ZoomControl, miniMapControl.UpdateVisibleAreaIndicator);
+
+            prop = DependencyPropertyDescriptor.FromProperty(ActualWidthProperty, typeof(ZoomControl));
+            if (e.OldValue != null)
+                prop.RemoveValueChanged(e.OldValue, miniMapControl.UpdateVisibleAreaIndicator);
+            prop.AddValueChanged(miniMapControl.ZoomControl, miniMapControl.UpdateVisibleAreaIndicator);
+
+            prop = DependencyPropertyDescriptor.FromProperty(ActualHeightProperty, typeof(ZoomControl));
+            if (e.OldValue != null)
+                prop.RemoveValueChanged(e.OldValue, miniMapControl.UpdateVisibleAreaIndicator);
+            prop.AddValueChanged(miniMapControl.ZoomControl, miniMapControl.UpdateVisibleAreaIndicator);
+        }
+
+        #endregion
+
+        public MiniMapControl()
+        {
+            InitializeComponent();
         }
 
         private void UpdateVisibleAreaIndicator(object sender, EventArgs e)
         {
-            VisibleAreaIndicator.Width = Math.Min(MiniMapContent.ActualWidth, MiniMapContent.ActualWidth / ZoomControl.Zoom);
-            VisibleAreaIndicator.Height = Math.Min(MiniMapContent.ActualHeight, MiniMapContent.ActualHeight / ZoomControl.Zoom);
+            if (ContentVisual == null || ZoomControl == null)
+                return;
 
-            double translateX = -ZoomControl.TranslateX*(MiniMapContent.ActualWidth/ZoomControl.ActualWidth);
-            double translateY = -ZoomControl.TranslateY*(MiniMapContent.ActualHeight/ZoomControl.ActualHeight);
+            Width = ZoomControl.ActualWidth * MiniMapScale;
+            Height = ZoomControl.ActualHeight * MiniMapScale;
+
+            double widthScale = ContentVisual.ActualWidth > 0.0 ? ZoomControl.ActualWidth / ContentVisual.ActualWidth : 1.0;
+            VisibleAreaIndicator.Width = Math.Min(MiniMapContent.ActualWidth, MiniMapContent.ActualWidth / ZoomControl.Zoom * widthScale);
+            double heightScale = ContentVisual.ActualHeight > 0.0 ? ZoomControl.ActualHeight / ContentVisual.ActualHeight : 1.0;
+            VisibleAreaIndicator.Height = Math.Min(MiniMapContent.ActualHeight, MiniMapContent.ActualHeight / ZoomControl.Zoom * heightScale);
+
+            double translateX = -ZoomControl.TranslateX*(MiniMapContent.ActualWidth/ContentVisual.ActualWidth);
+            double translateY = -ZoomControl.TranslateY*(MiniMapContent.ActualHeight/ContentVisual.ActualHeight);
 
             var transformGroup = new TransformGroup();
             transformGroup.Children.Add(new TranslateTransform(translateX, translateY));
